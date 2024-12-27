abdbc3534587d4c26e89f2a61698d5d3a9bd5664
AbdullohwebS <abdullohnurmuhammedov654@gmail.com>
2024-12-27 10:46:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// ! hello
+// ? hi
+// * bye
+// TODO: finish this function
+
+// var - eski va uni qayta qayta ishlayt sa bo'ladi. name larni ham almashtirsa bo'ladi uni kop ishlat sa xato qaytaradi  site mizda muommo larchiqaradi  
+// let - uni ham qyata qayta ishlatsa bo'ladi u yangiro va qulay ishlatishga bunda qancha kob yozsaham xato chiqarmaydi
+// const - esa u o'zgarmas uni qayta qayta ishlat sa bo'medi. 
+
+// var - 4    
+// let - 5 
+// const - 3
+
+
+// prompt 
+
+// let ism = prompt('Abdulloh');
+// console.log(ism);
+
+
+// let ism = prompt("Abdulloh");
+
+// let birinChiharf = ism.charAt().toUpperCase();
+// let davomi = ism.slice(1).toLowerCase();
+// let resault = birinChiharf + davomi;
+
+// console.log(resault);
+
+
+// charAt()
+// slice()
+// toUpperCase()
+// toLowercase()
+
+
